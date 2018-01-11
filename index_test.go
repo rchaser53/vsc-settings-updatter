fbad54cae27e0de756c41427ce13a0c278303140
rchaser53 <tayoshizawa29@gmail.com>
2018-01-11 23:41:48 +0900

    add samePathError test

--- index_test.go
+++ index_test.go
@@ -6,23 +6,44 @@ import (
 	customError "vscSettingUpdatter/error"
 )
 
-type MockStruct struct{}
+type IOErrorMockStruct struct{}
 
-func (m MockStruct) Bool(key string) bool {
+func (m IOErrorMockStruct) Bool(key string) bool {
 	return true
 }
 
-func (m MockStruct) String(key string) string {
+func (m IOErrorMockStruct) String(key string) string {
 	return "test"
 }
 
 func TestIOError(t *testing.T) {
-	err := ExecCli(MockStruct{})
+	err := ExecCli(IOErrorMockStruct{})
 
 	switch err.(type) {
 	case customError.IoError:
 		os.Exit(0)
 	default:
 		t.Error("err should be IoError")
+	}
+}
+
+type SamePathErrorMockStruct struct{}
+
+func (m SamePathErrorMockStruct) Bool(key string) bool {
+	return false
+}
+
+func (m SamePathErrorMockStruct) String(key string) string {
+	return "test"
+}
+
+func TestSamePathError(t *testing.T) {
+	err := ExecCli(SamePathErrorMockStruct{})
+
+	switch err.(type) {
+	case customError.SamePathError:
+		os.Exit(0)
+	default:
+		t.Error("err should be SamePathError")
 	}
 }
