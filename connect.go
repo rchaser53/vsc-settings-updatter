5fb8f26b11a809849dd55d1ccee18ec0fecef49e
rchaser53 <tayoshizawa29@gmail.com>
2018-01-13 21:09:39 +0900

    try to use di in go

--- connect.go
+++ connect.go
@@ -7,27 +7,31 @@ import (
 )
 
 type Connect interface {
-	Get(url string) (http.Response, error)
+	Get(url string) (*http.Response, error)
 }
 
-func PullSettigsJson(url string, destPath string) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return NetworkError{Msg: err.Error()}
-	}
+func createPullSettingsJson(connect Connect) func(string, string) error {
+	return func(url string, destPath string) error {
+		resp, err := connect.Get(url)
+		if err != nil {
+			return NetworkError{Msg: err.Error()}
+		}
 
-	byteArray, err := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-	if err != nil {
-		return IoError{Msg: err.Error()}
-	}
+		byteArray, err := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		if err != nil {
+			return IoError{Msg: err.Error()}
+		}
 
-	file, err := os.Create(destPath)
-	if err != nil {
-		return IoError{Msg: err.Error()}
-	}
-	defer file.Close()
-	file.Write(byteArray)
+		file, err := os.Create(destPath)
+		if err != nil {
+			return IoError{Msg: err.Error()}
+		}
+		defer file.Close()
+		file.Write(byteArray)
 
-	return nil
+		return nil
+	}
 }
+
+var PullSettigsJson = createPullSettingsJson(&http.Client{})
